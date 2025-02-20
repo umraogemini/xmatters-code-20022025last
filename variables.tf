86f4541df2ba6f84f07cee6012dda2dbf9206e76
umraogemini <umraogemini@gmail.com>
2025-02-20 16:37:28 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "europe-west2"
+}
+
+variable "xmatters_webhook_url" {
+  description = "Webhook URL for xMatters integration"
+  type        = string
+  default     = "https://hap.bank.com/api/sendAlert?@aboutSource=GCP"
+}
+
+variable "notification_email" {
+  description = "Email address for receiving alerts"
+  type        = string
+  default     = "etfineexpeakdeliveryteamitsupport@noexternalmail.hsbc.com"
+}
