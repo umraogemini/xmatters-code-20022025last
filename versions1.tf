f6fcbc8d543f197434378bfbe49998988647673f
umraogemini <umraogemini@gmail.com>
2025-05-06 11:38:52 +0530

    Create versions1.tf

---
+++ versions1.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_version = "~> 1.11.1"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.13.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.88.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.12.1"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "2.3.4"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.0"
+    }
+    venafi = {
+      source  = "registry.terraform.io/Venafi/venafi"
+      version = "0.21.2"
+    }
+    googlecompute = {
+      source  = "PK/GoogleCompute"
+      version = "1.1.8"
+    }
+    ansible = {
+      source  = "PK/Ansible"
+      version = "1.1.3"
+    }
+  }
+}
