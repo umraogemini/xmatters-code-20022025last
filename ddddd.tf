9e25656d7ba7bb20085992805d4c4970b49c4f87
umraogemini <umraogemini@gmail.com>
2025-05-25 20:22:05 +0530

    Update ddddd.tf

--- ddddd.tf
+++ ddddd.tf
@@ -16,3 +16,11 @@ documentation {
 EOT
   mime_type = "text/markdown"
 }
+
+
+
+"text": "${var.project_id} VM High CPU Utilization Alert. ${summary} ${policy.display_name} ${condition.display_name} ${resource.label.instance_id} ${resource.label.instance_name} ${resource.label.zone} ${resource.type} ${metric.type} ${value} ${threshold_value} ${start_time}",
+
+
+
+"text": "${var.project_id} VM High CPU Utilization Alert. Summary: ${summary}, Policy: ${policy.display_name}, Condition: ${condition.display_name}, Instance ID: ${resource.label.instance_id}, Instance Name: ${resource.label.instance_name}, Zone: ${resource.label.zone}, Resource Type: ${resource.type}, Metric Type: ${metric.type}, CPU Utilization: ${value}, Threshold: ${threshold_value}, Start Time: ${start_time}",
