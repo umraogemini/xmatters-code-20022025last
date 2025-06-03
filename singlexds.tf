d062efb9e10fb8fa9aa63b9dd80eb459383edd19
umraogemini <umraogemini@gmail.com>
2025-06-03 17:36:32 +0530

    Create singlexds.tf

---
+++ singlexds.tf
@@ -0,0 +1,14 @@
+resource.type="k8s_container"
+(severity="ERROR" OR severity="INFO")
+(
+  textPayload:"RD10001" OR
+  textPayload:"RD10014" OR
+  textPayload:"RD10015" OR
+  textPayload:"RD10016" OR
+  textPayload:"RD10024" OR
+  textPayload:"RD10034" OR
+  textPayload:"RD10035" OR
+  textPayload:"RD10044" OR
+  textPayload:"RD10094" OR
+  textPayload:"RD10099"
+)
